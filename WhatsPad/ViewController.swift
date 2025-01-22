ef52e9770e9c6dadb48e7654e4cf7d6899b1153e
a7maadf <a7maadf.github.uawca@slmail.me>
2025-01-22 09:53:46 -0600

    Initial Commit

---
+++ WhatsPad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsPad
+//
+//  Created by Ahmad Salem on 1/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WhatsPadTests/WhatsPadTests.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsPadTests.swift
+//  WhatsPadTests
+//
+//  Created by Ahmad Salem on 1/22/25.
+//
+
+import Testing
+@testable import WhatsPad
+
+struct WhatsPadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
